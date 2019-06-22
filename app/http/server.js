fe45d950fe95d39b5f88f6bb6bbd6961000fffb8
Craig Fay <craigfay1@gmail.com>
2019-06-22 14:45:55 -0400

    Use nodemon in npm run start

--- app/http/server.js
+++ app/http/server.js
@@ -1,8 +1,9 @@
 const { HttpServer, HttpResponse } = require('../../_core/dist/main')
 
 const s = new HttpServer({ port: 4000 });
-s.route('GET', '/', (request) => {
-  return new HttpResponse({ body: 'Hello?' });
+s.route('GET', '/*', (request) => {
+  console.log('Getting a request ... \n', request);
+  return new HttpResponse({ body: 'Hello!' });
 });
 
 s.listen();
