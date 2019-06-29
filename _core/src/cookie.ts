c67c18c85725495a93f9021de6d4715edef36ce3
Craig Fay <craigfay1@gmail.com>
2019-06-29 15:05:01 -0500

    WIP maybe change cookie format

--- _core/src/cookie.ts
+++ _core/src/cookie.ts
@@ -1,8 +1,10 @@
 import { strict as assert } from 'assert';
 
 export interface Cookie {
-  name: string;
-  value: string;
+  [name:string]: [string, CookieOptions?]
+}
+
+export interface CookieOptions {
   expires?: Date | number;
   maxAge?: number;
   domain?: string;
@@ -36,60 +38,68 @@ export function parse(cookies: string): object {
  * Stringify an arbitrary amount of cookie objects 
  * @param cookies
  */
-export function stringify(...cookies: Cookie[]) {
+export function stringify(cookie: Cookie) {
 
-  return cookies.map(cookie => {
-    const value = encodeURIComponent(String(cookie.value))
+  const names = Object.keys(cookie);
+
+  return names.map(name => {
+    let [value, options] = cookie[name];
+
+    if (!options) {
+      options = {};
+    }
+
+    const encodedValue = encodeURIComponent(String(value))
       .replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
 
-    const name = encodeURIComponent(String(cookie.name))
+    const encodedName = encodeURIComponent(String(name))
       .replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent)
       .replace(/[\(\)]/g, escape);
 
     // ref: https://tools.ietf.org/html/draft-ietf-httpbis-cookie-prefixes-00#section-3.1
-    if (name.startsWith("__Secure")) {
-      cookie.secure = true;
+    if (encodedName.startsWith("__Secure")) {
+      options.secure = true;
     }
-    if (name.startsWith("__Host")) {
-      cookie.path = "/";
-      cookie.secure = true;
-      delete cookie.domain;
+    if (encodedName.startsWith("__Host")) {
+      options.path = "/";
+      options.secure = true;
+      delete options.domain;
     }
 
-    let out = [`${name}=${value}`];
+    let out = [`${encodedName}=${encodedValue}`];
 
-    if (cookie.secure) {
+    if (options.secure) {
       out.push("Secure");
     }
-    if (cookie.httpOnly) {
+    if (options.httpOnly) {
       out.push("HttpOnly");
     }
-    if (typeof cookie.maxAge == 'number') {
-      assert(Number.isInteger(cookie.maxAge) && cookie.maxAge > 0, "Max-Age must be a positive integer");
-      out.push(`Max-Age=${cookie.maxAge}`);
+    if (typeof options.maxAge == 'number') {
+      assert(Number.isInteger(options.maxAge) && options.maxAge > 0, "Max-Age must be a positive integer");
+      out.push(`Max-Age=${options.maxAge}`);
     }
-    if (cookie.domain) {
-      assert(typeof cookie.domain == 'string', 'Domain must be of type String');
-      out.push(`Domain=${cookie.domain}`);
+    if (options.domain) {
+      assert(typeof options.domain == 'string', 'Domain must be of type String');
+      out.push(`Domain=${options.domain}`);
     }
-    if (cookie.sameSite) {
-      assert(typeof cookie.sameSite == 'string', 'sameSite must be of type String');
-      out.push(`SameSite=${cookie.sameSite}`);
+    if (options.sameSite) {
+      assert(typeof options.sameSite == 'string', 'sameSite must be of type String');
+      out.push(`SameSite=${options.sameSite}`);
     }
-    if (cookie.path) {
-      assert(typeof cookie.path == 'string', 'Path must be of type String');
-      out.push(`Path=${cookie.path}`);
+    if (options.path) {
+      assert(typeof options.path == 'string', 'Path must be of type String');
+      out.push(`Path=${options.path}`);
     }
-    if (cookie.expires) {
+    if (options.expires) {
       // Numbers will be converted into a date N days in the future
-      if (typeof cookie.expires === 'number') {
-        cookie.expires = new Date(+new Date() + cookie.expires * 864e+5);
+      if (typeof options.expires === 'number') {
+        options.expires = new Date(+new Date() + options.expires * 864e+5);
       }
-      assert(cookie.expires instanceof Date, "Cookie expiration cannot be converted to a valid Date object")
-      out.push(`Expires=${cookie.expires.toUTCString()}`);
+      assert(options.expires instanceof Date, "Cookie expiration cannot be converted to a valid Date object")
+      out.push(`Expires=${options.expires.toUTCString()}`);
     }
-    if (cookie.unparsed) {
-      out.push(cookie.unparsed.join("; "));
+    if (options.unparsed) {
+      out.push(options.unparsed.join("; "));
     }
 
     return out.join("; ");
