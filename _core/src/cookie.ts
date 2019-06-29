bed24f191c71ec394c5bd63f67028024a465dd39
Craig Fay <craigfay1@gmail.com>
2019-06-29 15:28:51 -0500

    Fulfill test requirements

--- _core/src/cookie.ts
+++ _core/src/cookie.ts
@@ -1,10 +1,8 @@
 import { strict as assert } from 'assert';
 
 export interface Cookie {
-  [name:string]: [string, CookieOptions?]
-}
-
-export interface CookieOptions {
+  name: string;
+  value: string;
   expires?: Date | number;
   maxAge?: number;
   domain?: string;
@@ -18,17 +16,26 @@ export interface CookieOptions {
 export type SameSite = "Strict" | "Lax";
 
 /**
- * Parse the "Cookies" header value into an object
+ * Parse the "Cookies" header value into a list of Cookie objects
  * @param cookies
  */
 export function parse(cookies: string): object {
-  const out = {};
+  const out = [];
+
   if (typeof cookies === 'string') {
     const c = cookies!.split(";");
     for (const kv of c) {
-      const cookieVal = kv.split("=");
-      const key = cookieVal.shift()!.trim();
-      out[key] = cookieVal.join("=");
+      const parsed:Cookie = { name: undefined, value: undefined }
+      let [name, details] = kv.split('=');
+      const [value, ...options] = details.split(';');
+      
+      if (name && value) {
+        parsed.name = name.trim();
+        parsed.value = value;
+        out.push(parsed);
+      }
+
+      // @TODO parse options
     }
   }
   return out;
@@ -38,68 +45,60 @@ export function parse(cookies: string): object {
  * Stringify an arbitrary amount of cookie objects 
  * @param cookies
  */
-export function stringify(cookie: Cookie) {
+export function stringify(...cookies: Cookie[]) {
 
-  const names = Object.keys(cookie);
-
-  return names.map(name => {
-    let [value, options] = cookie[name];
-
-    if (!options) {
-      options = {};
-    }
-
-    const encodedValue = encodeURIComponent(String(value))
+  return cookies.map(cookie => {
+    const value = encodeURIComponent(String(cookie.value))
       .replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
 
-    const encodedName = encodeURIComponent(String(name))
+    const name = encodeURIComponent(String(cookie.name))
       .replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent)
       .replace(/[\(\)]/g, escape);
 
     // ref: https://tools.ietf.org/html/draft-ietf-httpbis-cookie-prefixes-00#section-3.1
-    if (encodedName.startsWith("__Secure")) {
-      options.secure = true;
+    if (name.startsWith("__Secure")) {
+      cookie.secure = true;
     }
-    if (encodedName.startsWith("__Host")) {
-      options.path = "/";
-      options.secure = true;
-      delete options.domain;
+    if (name.startsWith("__Host")) {
+      cookie.path = "/";
+      cookie.secure = true;
+      delete cookie.domain;
     }
 
-    let out = [`${encodedName}=${encodedValue}`];
+    let out = [`${name}=${value}`];
 
-    if (options.secure) {
+    if (cookie.secure) {
       out.push("Secure");
     }
-    if (options.httpOnly) {
+    if (cookie.httpOnly) {
       out.push("HttpOnly");
     }
-    if (typeof options.maxAge == 'number') {
-      assert(Number.isInteger(options.maxAge) && options.maxAge > 0, "Max-Age must be a positive integer");
-      out.push(`Max-Age=${options.maxAge}`);
+    if (typeof cookie.maxAge == 'number') {
+      assert(Number.isInteger(cookie.maxAge) && cookie.maxAge > 0, "Max-Age must be a positive integer");
+      out.push(`Max-Age=${cookie.maxAge}`);
     }
-    if (options.domain) {
-      assert(typeof options.domain == 'string', 'Domain must be of type String');
-      out.push(`Domain=${options.domain}`);
+    if (cookie.domain) {
+      assert(typeof cookie.domain == 'string', 'Domain must be of type String');
+      out.push(`Domain=${cookie.domain}`);
     }
-    if (options.sameSite) {
-      assert(typeof options.sameSite == 'string', 'sameSite must be of type String');
-      out.push(`SameSite=${options.sameSite}`);
+    if (cookie.sameSite) {
+      assert(typeof cookie.sameSite == 'string', 'sameSite must be of type String');
+      out.push(`SameSite=${cookie.sameSite}`);
     }
-    if (options.path) {
-      assert(typeof options.path == 'string', 'Path must be of type String');
-      out.push(`Path=${options.path}`);
+    if (cookie.path) {
+      assert(typeof cookie.path == 'string', 'Path must be of type String');
+      out.push(`Path=${cookie.path}`);
     }
-    if (options.expires) {
+    if (cookie.expires) {
       // Numbers will be converted into a date N days in the future
-      if (typeof options.expires === 'number') {
-        options.expires = new Date(+new Date() + options.expires * 864e+5);
+      if (typeof cookie.expires === 'number') {
+        cookie.expires = new Date(+new Date() + cookie.expires * 864e+5);
       }
-      assert(options.expires instanceof Date, "Cookie expiration cannot be converted to a valid Date object")
-      out.push(`Expires=${options.expires.toUTCString()}`);
+      assert(cookie.expires instanceof Date, "Cookie expiration cannot be converted to a valid Date object")
+      out.push(`Expires=${cookie.expires.toUTCString()}`);
     }
-    if (options.unparsed) {
-      out.push(options.unparsed.join("; "));
+    if (cookie.unparsed) {
+      out.push(cookie.unparsed.join("; "));
     }
 
     return out.join("; ");
