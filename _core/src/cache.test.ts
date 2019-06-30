bda88a5205e14d2ad2ad7cd1f10187cafd57ba28
Craig Fay <craigfay1@gmail.com>
2019-06-30 18:14:59 -0500

    WIP add test coverage for new cache properties

--- _core/src/cache.test.ts
+++ _core/src/cache.test.ts
@@ -3,23 +3,45 @@ import { Cache } from './cache';
 
 export const tests = [
   cacheConstructTest,
+  cacheDeleteAllTest,
+  cacheSetTest,
 ]
 
 async function cacheConstructTest() {
   try {
     const cache = new Cache({ address: process.env.REDIS_HOST });
-    assert.equal(
-      typeof cache,
-      'object',
-    )
-    assert.equal(
-      cache instanceof Promise,
-      false,
-    )
+    assert.equal(typeof cache, 'object');
+    assert.equal(cache instanceof Promise, false);
     assert(typeof cache.get == 'function');
     assert(typeof cache.set == 'function');
     assert(typeof cache.keys == 'function');
+    assert(typeof cache.delete == 'function');
+    assert(typeof cache.deleteAll == 'function');
     assert(typeof cache.close == 'function');
+    await cache.close();
+  }
+  catch (e) {
+    return e;
+  }
+}
+
+async function cacheDeleteAllTest() {
+  try {
+    const cache = new Cache({ address: process.env.REDIS_HOST });
+    assert(await cache.deleteAll());
+    await cache.close();
+  }
+  catch (e) {
+    return e;
+  }
+}
+
+async function cacheSetTest() {
+  try {
+    const cache = new Cache({ address: process.env.REDIS_HOST });
+
+    assert(await cache.set('Time', 'Traveler'))
+    assert(await cache.set('for', JSON.stringify(['it', 'will', 'be', 'convenient'])));
 
     await cache.close();
 
