65df61c13d3a571b3de2cc16488f6394aa735947
Craig Fay <craigfay1@gmail.com>
2019-07-01 22:14:03 -0500

    Update cache.types.ts

--- _core/src/cache.types.ts
+++ _core/src/cache.types.ts
@@ -2,6 +2,8 @@ export interface CacheInterface {
   get: (key:string) => Promise<string>;
   set: (key: string, val: string, options?: SetOptionsInterface) => Promise<boolean>;
   keys: () => Promise<string[]>;
+  delete: (key:string) => Promise<boolean>;
+  deleteAll: () => Promise<boolean>;
   close: () => void;
 }
 
