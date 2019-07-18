943dd972b2fed398ea743a5058419a757398dad6
Craig Fay <craigfay1@gmail.com>
2019-07-18 00:06:01 -0500

    Refactor string field

--- _core/src/models.ts
+++ _core/src/models.ts
@@ -7,54 +7,41 @@ function Field(name) {
   this.test = (val=null) => this.constraints.forEach(c => c(val));
 }
 
+const notNull = (name, val) => {
+  if (val == null)
+  throw new Error(`${name} must not be null`);
+}
+const minLength = (name, val, arg) => {
+  if (val.length < arg)
+  throw new Error(`${name} has a min length of ${arg}`)
+}
+const maxLength = (name, val, arg) => {
+  if (val.length > arg)
+  throw new Error(`${name} has a max length of ${arg}`)
+}
+const alphabetical = (name, val) => {
+  if (false == /^[a-zA-Z]+$/.test(val))
+  throw new Error(`${name} must only use alphabetical characters`);
+}
+const numeric = (name, val) => {
+  if (false == /^\d+$/.test(val))
+  throw new Error(`${name} must only use numeric characters`);
+}
+
 export function string(name) {
   let f: any = new Field(name);
   f.constraints.push(val => assert(val === null || typeof val == 'string'));
 
-  f.notNull = () => {
-    f.constraints.push(val => {
-      if (val == null)
-      throw new Error(`${name} must not be null`);
-    })
+  f.constrain = (fn, ...args) => {
+    f.constraints.push(val => fn(name, val, ...args));
     return f;
   }
 
-  f.minLength = len => {
-    f.constraints.push(val => {
-      if (val.length < len)
-      throw new Error(`${name} has a min length of ${len}`);
-    });
-    return f;
-  }
-
-  f.maxLength = len => {
-    f.constraints.push(val => {
-      if (val.length > len)
-      throw new Error(`${name} has a max length of ${len}`);
-    });
-    return f;
-  }
-
-  f.alphabetical = () => {
-    f.constraints.push(val => {
-      if (false == /^[a-zA-Z]+$/.test(val))
-      throw new Error(`${name} must only use alphabetical characters`);
-    })
-    return f;
-  }
-
-  f.numeric = () => {
-    f.constraints.push(val => {
-      if (false == /^\d+$/.test(val))
-      throw new Error(`${name} must only use numeric characters`);
-    })
-    return f;
-  }
-
-  f.constrain = fn => {
-    f.constraints.push(val => fn(name, val));
-    return f;
-  }
+  f.notNull = () => f.constrain(notNull);
+  f.minLength = arg => f.constrain(minLength, arg)
+  f.maxLength = arg => f.constrain(maxLength, arg)
+  f.alphabetical = () => f.constrain(alphabetical)
+  f.numeric = () => f.constrain(numeric)
 
   return f;
 }
