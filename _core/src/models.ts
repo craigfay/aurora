29af071110ee4c7ad652d29ef3b5377a95656d9f
Craig Fay <craigfay1@gmail.com>
2019-07-19 15:17:16 -0500

    Organize models module

--- _core/src/models.ts
+++ _core/src/models.ts
@@ -1,5 +1,27 @@
 import { strict as assert } from 'assert';
 
+/**
+ * Models can be used to define complex data structures
+ * that have logical constraints, in addition to type constraints
+ */
+
+/**
+ * Models are made up of Fields, which are made up of Constraints
+ * See models.test.ts for examples of how Models are composed
+ */
+
+export function Model(...fields) {
+  this.fields = {}
+  fields.forEach(f => {
+    const { name, constraints, test } = f;
+    this.fields[name] = { constraints, test };
+  });
+
+  this.test = obj => {
+    Object.keys(obj).forEach(name => this.fields[name].test(obj[name]));
+  }
+}
+
 function Field(name) {
   assert(typeof name == 'string');
   this.name = name;
@@ -7,13 +29,22 @@ function Field(name) {
   this.test = (val=null) => this.constraints.forEach(c => c(name, val));
 }
 
-const stringFieldType = () => function fieldType(name, val) {
-  if (val != null && typeof val != 'string')
-  throw new Error(`${name} must be a string. Received ${typeof val}`);
-}
+/**
+ * Generic constraints that are available to multiple field types
+ */
+
 const genericNotNull = () => function notNull(name, val) {
   if (val == null)
   throw new Error(`${name} must not be null`);
+}
+
+/**
+ * Constraints available to string fields
+ */
+
+const stringFieldType = () => function fieldType(name, val) {
+  if (val != null && typeof val != 'string')
+  throw new Error(`${name} must be a string. Received ${typeof val}`);
 }
 const stringLength = arg => function length(name, val) {
   if (val.length != arg)
@@ -36,21 +67,9 @@ const stringNumeric = () => function numeric(name, val) {
   throw new Error(`${name} must only use numeric characters`);
 }
 
-export function string(name) {
-  let f: any = new Field(name);
-  f.constrain = fn => {
-    f.constraints.push(fn)
-    return f;
-  }
-  f.constrain(stringFieldType());
-  f.notNull = () => f.constrain(genericNotNull());
-  f.length = arg => f.constrain(stringLength(arg));
-  f.minLength = arg => f.constrain(stringMinLength(arg));
-  f.maxLength = arg => f.constrain(stringMaxLength(arg));
-  f.alphabetical = () => f.constrain(stringAlphabetical())
-  f.numeric = () => f.constrain(stringNumeric())
-  return f;
-}
+/**
+ * Constraints available to integer fields
+ */
 
 const integerFieldType = () => function fieldType(name, val) {
   if (val != null && !Number.isInteger(val))
@@ -70,6 +89,30 @@ const integerRange = (...args) => function range(name, val) {
   throw new Error(`${name} must be between ${min} and ${max}`)
 }
 
+/**
+ * String Field
+ */
+
+export function string(name) {
+  let f: any = new Field(name);
+  f.constrain = fn => {
+    f.constraints.push(fn)
+    return f;
+  }
+  f.constrain(stringFieldType());
+  f.notNull = () => f.constrain(genericNotNull());
+  f.length = arg => f.constrain(stringLength(arg));
+  f.minLength = arg => f.constrain(stringMinLength(arg));
+  f.maxLength = arg => f.constrain(stringMaxLength(arg));
+  f.alphabetical = () => f.constrain(stringAlphabetical())
+  f.numeric = () => f.constrain(stringNumeric())
+  return f;
+}
+
+/**
+ * Integer Field
+ */
+
 export function integer(name) {
   let f: any = new Field(name);
   f.constrain = fn => {
@@ -82,16 +125,4 @@ export function integer(name) {
   f.notZero = () => f.constrain(integerNotZero());
   f.range = (...args) => f.constrain(integerRange(...args))
   return f;
-}
-
-export function Model(...fields) {
-  this.fields = {}
-  fields.forEach(f => {
-    const { name, constraints, test } = f;
-    this.fields[name] = { constraints, test };
-  });
-
-  this.test = obj => {
-    Object.keys(obj).forEach(name => this.fields[name].test(obj[name]));
-  }
 }
